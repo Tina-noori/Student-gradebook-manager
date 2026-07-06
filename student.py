91225cde326a8d64d79cac7a850715b6f13a57fd
Tina-noori <tinanoori725@gmail.com>
2026-07-06 14:22:59 +0430

    Add getter & setter student class

--- student.py
+++ student.py
@@ -1,8 +1,23 @@
 class Student:
-    def __init__(self,student_id ,full_name ,email,course ):
+    def __init__(self,student_id ,nam ,email,course ):
         self.__student_id = student_id
-        self.__full_name = full_name
+        self.__full_name = nam
         self.__email = email
         self.course = []
 
-        
+    def get_id(self):
+        return self.__student_id
+
+    def get_nam(self):
+        return self.__full_name
+
+    def get_email(self):
+        return self.__email
+
+    def set_email(self,email):
+        if "@" in email:
+            self.__email = email
+        else:
+            print("Invalid Email address")
+            
+
