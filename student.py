72a685f37bf5914571dd2858986c1235a64ee633
Tina-noori <tinanoori725@gmail.com>
2026-07-06 14:01:35 +0430

    Add student class

---
+++ student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self,student_id ,full_name ,email,course ):
+        self.__student_id = student_id
+        self.__full_name = full_name
+        self.__email = email
+        self.course = []
+
+        
