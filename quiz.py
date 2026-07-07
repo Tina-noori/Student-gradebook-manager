8953172554410b117db674838f2784dfdf1e72fb
Tina-noori <tinanoori725@gmail.com>
2026-07-08 02:19:29 +0430

    Add method display-info quiz class

--- quiz.py
+++ quiz.py
@@ -2,3 +2,6 @@ from assessment import Assessment
 class Quiz(Assessment):
     def __init__(self, title, max_score):
         super().__init__(title, max_score)
+
+    def display_info(self):
+        print(f"Quiz : {self.title} - Max score: {self.max_score}")
