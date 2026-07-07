e43500503386163a472ea91347a57bc5c8ea74fb
Tina-noori <tinanoori725@gmail.com>
2026-07-08 03:11:48 +0430

    Add display-info = Override

--- exam.py
+++ exam.py
@@ -2,4 +2,6 @@ from assessment import Assessment
 class Exam(Assessment):
     def __init__(self,title,max_score):
         super().__init__(title,max_score)
-        
+
+    def display_info(self):
+        print(f"Exam : {self.title} - Max Score: {self.max_score}")
