48a6c6d8b78f901f6f457960f5efbf38acdc056a
Tina-noori <tinanoori725@gmail.com>
2026-07-08 00:22:58 +0430

    Add method student-assessment-find assessment  class course

---
+++ assessment.py
@@ -0,0 +1,5 @@
+class Assessment:
+    def __init__(self, title,max_score):
+        self.title = title
+        self.max_score = max_score
+        self.students_score = {}
--- course.py
+++ course.py
@@ -4,4 +4,24 @@ class Course:
         self.course_name = course_name
         self.students = []
         self.assessments = []
-        
+
+    def add_student(self,student_id):
+        if student_id not in self.students:
+            self.students.append(student_id)
+        else:
+            print("Student already added")
+
+    def add_assessment(self,assessment):
+        self.assessments.append(assessment)
+
+    def find_assessment(self,title):
+        for assessment in self.assessments:
+            if assessment.title == title:
+                return assessment
+            return None
+
+
+
+
+
+
