d0de47165225632c3c1ba39cb4609c548653eada
Tina-noori <tinanoori725@gmail.com>
2026-07-08 04:15:01 +0430

    Add method grade-message = Overrides class project

--- project.py
+++ project.py
@@ -6,3 +6,15 @@ class Project (Assessment):
 
     def display_info(self):
         print(f"Project: {self.title} - Max Score: {self.max_score}")
+
+
+    def grade_message(self,score):
+        percentage =self.calculate_percentage(score)
+        if percentage is None:
+            return "Invalid Score"
+        elif percentage >= 90 :
+            return "Excellent project"
+        elif percentage >= 55 :
+            return "Project Complete"
+        else:
+            return "Project needs improvement"
