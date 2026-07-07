a2c6db22ea5edd253c9aa0b89c9b2cd1c66ebd1c
Tina-noori <tinanoori725@gmail.com>
2026-07-08 00:53:07 +0430

    Add method calculate score class assessment

--- assessment.py
+++ assessment.py
@@ -3,3 +3,9 @@ class Assessment:
         self.title = title
         self.max_score = max_score
         self.students_score = {}
+
+    def calculate_score(self,student_id,score):
+        if 0 <= score <= self.max_score:
+            self.students_score[student_id] = score
+        else:
+            print("Invalid score")
