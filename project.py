f769000356e4077dcd77febaa804b253f0f92b71
Tina-noori <tinanoori725@gmail.com>
2026-07-08 04:03:37 +0430

    Add method display-info = Overrides class project

--- project.py
+++ project.py
@@ -2,4 +2,7 @@ from assessment import Assessment
 class Project (Assessment):
     def __init__(self, title,max_score):
         super().__init__(title,max_score)
-        
+
+
+    def display_info(self):
+        print(f"Project: {self.title} - Max Score: {self.max_score}")
