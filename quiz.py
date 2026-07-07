db2a06fe9d9366b7d2be468ee78fa23c2b9cd45d
Tina-noori <tinanoori725@gmail.com>
2026-07-08 02:55:02 +0430

    Add method grade-massage = Overrides

--- quiz.py
+++ quiz.py
@@ -5,3 +5,18 @@ class Quiz(Assessment):
 
     def display_info(self):
         print(f"Quiz : {self.title} - Max score: {self.max_score}")
+
+
+    def grade_message(self,score):
+            percentage = self.calculate_percentage(score)
+            if percentage is None:
+                print("Invalid score")
+            elif percentage >= 90:
+                print("Great quiz result")
+            elif percentage >= 70:
+                print("Good effort on the quiz")
+            elif percentage >= 50:
+                print("Needs more practice")
+            else:
+                print("Quiz failed")
+                
