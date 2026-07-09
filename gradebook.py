a9d2cfea88d78ac3ea97c7ae2d01ea5045e5e216
Tina-noori <tinanoori725@gmail.com>
2026-07-10 00:36:19 +0430

    Add method add-student gradebook class

--- gradebook.py
+++ gradebook.py
@@ -3,5 +3,9 @@ class Gradebook:
     def __init__(self,passing_grade=55):
         self.students = []
         self.courses = []
-        self.grade =[]
+        self.grades =[]
         self.passing_grade = passing_grade
+
+    def add_student(self,student):
+        self.students[student.student.id] = student
+        self.grades[student.student.id] = {}
