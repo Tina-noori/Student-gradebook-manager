82e02d1e6a8f58339b679f41dace6308bee27d57
Tina-noori <tinanoori725@gmail.com>
2026-07-10 00:21:58 +0430

    Add gradebook class

---
+++ gradebook.py
@@ -0,0 +1,7 @@
+
+class Gradebook:
+    def __init__(self,passing_grade=55):
+        self.students = []
+        self.courses = []
+        self.grade =[]
+        self.passing_grade = passing_grade
